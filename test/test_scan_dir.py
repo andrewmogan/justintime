1e7e38ecb97ba0202b538a9330775f054e44e21d
Alessandro Thea <alessandro.thea@cern.ch>
2023-02-27 15:17:14 +0100

    Adding test script

---
+++ test/test_scan_dir.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from justintime.cruncher.datamanager import DataManager
+import sys
+import rich
+import logging
+import click
+import re
+from rich import print
+from pathlib import Path
+from itertools import groupby
+from collections import defaultdict
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-i', '--interactive', is_flag=True, default=False)
+@click.argument('dir_path', type=click.Path(exists=True))
+
+def cli(interactive: bool, dir_path: str) -> None:
+
+    # rdm = DataManager(dp.parent, 'ProtoWIB', 'VDColdbox')
+    rdm = DataManager(dir_path, 'WIB', 'HDColdbox')
+
+    re_app_run = re.compile('(.*)_run(\d*)')
+
+    lf = rdm.list_files()
+    gf_it = groupby(lf, lambda x: re_app_run.match(x).groups())
+    gf = {k: [x for x in d if x] for k,d in gf_it}
+    a = defaultdict(dict)
+
+    for k,v in gf.items():
+        a[k[0]][int(k[1])]=v
+
+    print(a)
+
+    if interactive:
+        import IPython
+        IPython.embed(colors="neutral")
+
+
+
+if __name__ == "__main__":
+    from rich.logging import RichHandler
+
+    logging.basicConfig(
+        level="DEBUG",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
+
+    cli()
