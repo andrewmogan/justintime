3b2b895a650535e768ec897eee4fdae863723433
Angeliki Mytilinaki <amytilin@ral-fpga-forge-01.dyndns.cern.ch>
2023-03-09 16:47:15 +0100

    Changes to control

--- src/justintime/controls/content/03_refresh_ctrl.py
+++ src/justintime/controls/content/03_refresh_ctrl.py
@@ -8,22 +8,16 @@ def return_obj(dash_app, engine):
     data=engine.get_session_run_files_map()
     ctrl_div = html.Div([html.Button('Refresh Files', id=ctrl_id, n_clicks=0)])
     ctrl = ctrl_class.ctrl("refresh_button", ctrl_id, ctrl_div, engine)
-    
-    init_callbacks(dash_app,engine,data )
-	
+    init_callbacks(dash_app, engine,data)
     return(ctrl)
-
+    
 def init_callbacks(dash_app, engine,data):
+    
     @dash_app.callback(
-        Output('partition_select_ctrl', 'options'),
-       
-        Input('03_refresh_ctrl', 'n_clicks'))
-    def update_file_list(pathname):
+		Output('partition_select_ctrl', 'options'),
+		Input('03_refresh_ctrl', 'n_clicks'))
         
-        fl = sorted(data.keys(), reverse=True)
-        logging.debug(f"Updated partition list: {fl}")
-        opts = [{'label': f, 'value': f} for f in data.keys()]
-        
-        return (
-            opts
-        )
+    def update_file_list(n_clicks):
+        data=engine.get_session_run_files_map()
+        opts = list(data.keys())
+        return (opts)
