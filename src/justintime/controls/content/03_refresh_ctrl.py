77d6b3d46cc8d44dfbebdf24d1f92919cd3aeed7
Alessandro Thea <alessandro.thea@cern.ch>
2023-03-09 17:45:24 +0100

    Adding refresh button

--- src/justintime/controls/content/03_refresh_ctrl.py
+++ src/justintime/controls/content/03_refresh_ctrl.py
@@ -5,19 +5,18 @@ from .. import ctrl_class
 
 def return_obj(dash_app, engine):
     ctrl_id = "03_refresh_ctrl"
-    data=engine.get_session_run_files_map()
+   
     ctrl_div = html.Div([html.Button('Refresh Files', id=ctrl_id, n_clicks=0)])
     ctrl = ctrl_class.ctrl("refresh_button", ctrl_id, ctrl_div, engine)
-    init_callbacks(dash_app, engine,data)
+    
     return(ctrl)
     
 def init_callbacks(dash_app, engine,data):
-    
     @dash_app.callback(
-		Output('partition_select_ctrl', 'options'),
-		Input('03_refresh_ctrl', 'n_clicks'))
-        
-    def update_file_list(n_clicks):
+        Output('session_run_files_map', 'data'),
+        Input('03_refresh_ctrl', 'n_clicks')
+        )
+    def update_file_list(n_clicks):  
         data=engine.get_session_run_files_map()
-        opts = list(data.keys())
-        return (opts)
+        print(data)
+        return (data)
