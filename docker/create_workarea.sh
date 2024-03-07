b486ff66ffde9858cf1fba7fe00f70719dab5908
Andrew Mogan <amogan@fnal.gov>
2024-03-07 10:47:58 -0600

    Streamline and reduce hard-coding
    - Add usage
    - Use provided release name to get DBT_VERSION via sym link
    - Modernize logic for release candidates (the naming convention changed
      recently)
    - Remove hard-coded spack environment setup; setup_dbt takes care of this

--- docker/create_workarea.sh
+++ docker/create_workarea.sh
@@ -1,30 +1,44 @@
-DUNEDAQ_RELEASE=$1
-if [[ "$DUNEDAQ_RELEASE" =~ ^(fd|nd)daq-v4.1.0 ]]; then
-    DBT_CREATE_OPTS=""
-    DBT_VERSION=$DUNEDAQ_RELEASE
-elif [[ "$DUNEDAQ_RELEASE" = "rc-"* ]]; then
-    DBT_CREATE_OPTS="-b candidate"
-    X=${DUNEDAQ_RELEASE/rc-/}
-    DBT_VERSION=dunedaq-${X/-*/}
+#!/bin/bash
+usage() {
+    echo "Usage: $0 <dune|fd|nd>daq-<version>-<a9|c8>"
+    echo "where the <version> format is vX.Y.Z"
+    exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "You must provide a release name as a single argument"
+    usage
 fi
 
-echo "------------------------------------------"
-echo "Setting up spack python"
-echo "------------------------------------------"
-source /cvmfs/dunedaq.opensciencegrid.org/spack/externals/stable/spack-0.18.1-gcc-12.1.0/spack-installation/share/spack/setup-env.sh
-spack load python@3.10.4
+DUNEDAQ_RELEASE=$1
+#if [[ "$DUNEDAQ_RELEASE" =~ ^(fd|nd)daq-v4.3.0-a9 ]]; then
+#    DBT_CREATE_OPTS=""
+#    DBT_VERSION=$DUNEDAQ_RELEASE
+if [[ "$DUNEDAQ_RELEASE" = *"rc-"* ]]; then
+    DBT_CREATE_OPTS="-b candidate"
+fi
+
+# Parse necessary dbt version from the release symbolic link
+DBT_DIR=/cvmfs/dunedaq.opensciencegrid.org/tools/dbt/
+if [ -e ${DBT_DIR}/$1 ]; then
+    DBT_VERSION=$(readlink -f ${DBT_DIR}/$1 | rev | cut -d'/' -f1 | rev)
+    echo "DBT version for $RELEASE: $DBT_VERSION"
+else
+    echo "Error: Symbolic link not found for $1 in $DBT_DIR."
+    exit 1
+fi
 
 source /cvmfs/dunedaq.opensciencegrid.org/setup_dunedaq.sh
-
 echo "------------------------------------------"
 echo "Loading daq-buildtool environment"
 echo "------------------------------------------"
+echo "DBT version: $DBT_VERSION"
 setup_dbt ${DBT_VERSION}
 
 echo "------------------------------------------"
 echo "Loading daq-release workarea"
 echo "------------------------------------------"
-dbt-create -c ${DBT_CREATE_OPTS} ${DUNEDAQ_RELEASE} dunedaq-area
+dbt-create ${DBT_CREATE_OPTS} ${DUNEDAQ_RELEASE} dunedaq-area
 
 cd dunedaq-area
 source ./env.sh
