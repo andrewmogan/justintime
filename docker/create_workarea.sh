a1ba9b166e6a97b2cdce1e9fbc411f291cd27719
Andrew Mogan <amogan@fnal.gov>
2024-03-07 12:22:32 -0600

    Fix path-finding in cvmfs

--- docker/create_workarea.sh
+++ docker/create_workarea.sh
@@ -2,6 +2,7 @@
 usage() {
     echo "Usage: $0 <dune|fd|nd>daq-<version>-<a9|c8>"
     echo "where the <version> format is vX.Y.Z"
+    echo "For a candidate release, the format is <dune|fd|nd>daq-<version>-rc<iteration>-<a9|c8>"
     exit 1
 }
 
@@ -11,20 +12,20 @@ if [ "$#" -ne 1 ]; then
 fi
 
 DUNEDAQ_RELEASE=$1
-#if [[ "$DUNEDAQ_RELEASE" =~ ^(fd|nd)daq-v4.3.0-a9 ]]; then
-#    DBT_CREATE_OPTS=""
-#    DBT_VERSION=$DUNEDAQ_RELEASE
+# "rc" denotes a candidate release, as opposed to frozen
 if [[ "$DUNEDAQ_RELEASE" = *"rc-"* ]]; then
     DBT_CREATE_OPTS="-b candidate"
 fi
 
 # Parse necessary dbt version from the release symbolic link
 DBT_DIR=/cvmfs/dunedaq.opensciencegrid.org/tools/dbt/
-if [ -e ${DBT_DIR}/$1 ]; then
-    DBT_VERSION=$(readlink -f ${DBT_DIR}/$1 | rev | cut -d'/' -f1 | rev)
+DBT_RELEASE=$(echo $DUNEDAQ_RELEASE | cut -d'-' -f1-2)
+if [ -e ${DBT_DIR}/$DBT_RELEASE ]; then
+    DBT_VERSION=$(readlink -f ${DBT_DIR}/$DBT_RELEASE | rev | cut -d'/' -f1 | rev)
     echo "DBT version for $RELEASE: $DBT_VERSION"
 else
-    echo "Error: Symbolic link not found for $1 in $DBT_DIR."
+    echo "Error: Symbolic link not found for $DBT_RELEASE in $DBT_DIR."
+    usage
     exit 1
 fi
 
@@ -38,6 +39,7 @@ setup_dbt ${DBT_VERSION}
 echo "------------------------------------------"
 echo "Loading daq-release workarea"
 echo "------------------------------------------"
+set -e # If this next step errors, stop the script
 dbt-create ${DBT_CREATE_OPTS} ${DUNEDAQ_RELEASE} dunedaq-area
 
 cd dunedaq-area
