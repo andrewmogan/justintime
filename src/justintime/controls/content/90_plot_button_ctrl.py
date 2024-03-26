4783da8790c438c5e9dae0dc8c5fd65f43c17b02
Andrew Mogan <amogan@fnal.gov>
2024-03-26 14:11:55 -0500

    Comment button callback

--- src/justintime/controls/content/90_plot_button_ctrl.py
+++ src/justintime/controls/content/90_plot_button_ctrl.py
@@ -17,16 +17,17 @@ def return_obj(dash_app, engine, storage):
 
     ctrl = ctrl_class.ctrl("plot_button", ctrl_id, ctrl_div, engine)
 
-    init_callbacks(dash_app, engine, storage)
+    #init_callbacks(dash_app, engine, storage)
 
     return(ctrl)
 
 # Thought: something with using storage to get the stored trigger record and plot from there?
-def init_callbacks(dash_app, engine, storage):
-    @dash_app.callback(
-        Output('plots_div', 'children'),
-        Input('90_plot_button_ctrl', 'n_clicks')
-        )
-    def update_plots(n_clicks):  
-        logging.debug('Plot button clicked!')
-        return 'Hi'
+#def init_callbacks(dash_app, engine, storage):
+#    @dash_app.callback(
+#        Output('plots_div', 'children'),
+#        Output("90_plot_button_ctrl", "n_clicks"),
+#        Input('90_plot_button_ctrl', 'n_clicks')
+#        )
+#    def update_plots(n_clicks):  
+#        logging.debug('Plot button clicked!')
+#        return 'Hi'
